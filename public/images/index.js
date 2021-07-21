da8d85a6446ffa1cf890db2b3b8b036f821542da
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 20:07:03 -0700

    added images

---
+++ public/images/index.js
@@ -0,0 +1,33 @@
+import one from "./houses/one.jpg";
+import two from "./houses/two.jpg";
+import three from "./houses/three.jpg";
+import four from "./houses/four.jpg";
+import five from "./houses/five.jpg";
+import six from "./houses/six.jpg";
+import seven from "./houses/seven.jpg";
+import eight from "./houses/eight.jpg";
+import nine from "./houses/nine.jpg";
+import ten from "./houses/ten.jpg";
+import eleven from "./houses/eleven.jpg";
+import twelve from "./houses/twelve.jpg";
+import thirteen from "./houses/thirteen.jpg";
+import fourteen from "./houses/fourteen.jpg";
+import fifteen from "./houses/fifteen.jpg";
+
+export {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  eleven,
+  twelve,
+  thirteen,
+  fourteen,
+  fifteen,
+};
