0fc5cc6294118888ba8ac3744ae38e5f298e7cf6
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 20:09:09 -0700

    feat:adding supabase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
 import Head from "next/head";
+import Card from "@components/Card";
+import styled from "styled-components";
+
+//until you find out how to get the supabase images and lnk them to each row, you can get the supabase data, map, and then spread the images into it for each row o data
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Home() {
   return (
@@ -7,7 +17,27 @@ export default function Home() {
         <title>Next && Styled Components </title>
       </Head>
 
-      <h1>Let&apos;s go</h1>
+      <Card />
     </div>
   );
+}
+
+// const data = [{ id: 1, img, imgAlt, title, specs, location, price }];
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(200px, auto-fit));
+  gap: 2rem;
+`;
+
+export async function getServerSideProps() {
+  // const res = await fetch("https://api.pexels.com/v1/search?query=house", {
+  //   headers: {
+  //     Authorization: process.env.API_KEY,
+  //   },
+  // });
+
+  return {
+    props: {},
+  };
 }
